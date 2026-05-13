251383576b3b25271aa6756f43c66132d682f2d6
rosario-terranova-nearform <rosario.terranova@nearform.com>
2026-05-13 19:37:12 +0200

    refactor: update RootLayout to include screen options and remove unused index file

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Inventory() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Inventory</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
